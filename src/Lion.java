45a09dfea370ac7f14d3aafc519dae8d046636b0
CMiles9905 <60078979+CMiles9905@users.noreply.github.com>
2020-01-19 16:57:04 -0800

    Update Lion.java

--- src/Lion.java
+++ src/Lion.java
@@ -1,34 +1,59 @@
 import java.awt.*;
-import java.util.Random;
+import java.util.*;
 
-//ERROR: class Lion threw an  exception in its constructor. I do not know why.
 
 public class Lion extends Critter {
+    private int colorMoves;
+    Color lionColor;
+    Random rand = new Random();
 
-    private int moves;
-    private Random rand;
-    private int colorNumber = rand.nextInt(3);
-
-    public Lion() {
-
+    public Lion(){
+        colorMoves=0;//1,2,3
+        getColor();
     }
 
+    public Color getColor() {
+        //Randomly picks one of three colors (Color.RED, Color.GREEN, Color.BLUE) and uses that color for three moves,
+        // then randomly picks one of those colors again for the next three moves,
+        // then randomly picks another one of those colors for the next three moves, and so on.
+        if (colorMoves%3==0){ // set new color
+            int x=0;
+            while (x==0){
+                int i=rand.nextInt(3); //0.Red 1.Green 2.Black
+                if (i==0 && this.lionColor!=Color.RED){
+                    this.lionColor= Color.RED;
+                    x++;
+                } if (i==1 && lionColor!=Color.GREEN){
+                    this.lionColor=Color.GREEN;
+                    x++;
+                } if (i==2 && lionColor!=Color.BLUE){
+                    this.lionColor=Color.BLUE;
+                    x++;
+                }
+            }
 
-    public String toString(){
+        }
+        return lionColor;
+    }
+
+    public String toString() {
         return "L";
     }
 
-
-
-    public Action getMove(CritterInfo info){
-        moves++;
+    public Action getMove(CritterInfo info) {
+        /*
+        always infect if an enemy is in front,
+         otherwise if a wall is in front or to the right, then turn left,
+         otherwise if a fellow Lion is in front, then turn right, otherwise hop.
+        */
+        colorMoves++;
         if (info.getFront()==Neighbor.OTHER){
             return Action.INFECT;
-        }else if ((info.getFront()==Neighbor.WALL)||(info.getRight()==Neighbor.WALL)){
+        } else if (info.getFront()==Neighbor.WALL||info.getRight()==Neighbor.WALL){
             return Action.LEFT;
-        }else if (info.getFront()==Neighbor.SAME){
+        } else if (info.getFront()==Neighbor.SAME){
             return Action.RIGHT;
-        }else{
+        } else {
             return Action.HOP;
         }
     }
