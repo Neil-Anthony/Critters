04a29fb097e1cce59f3ffd998cd6884a3c21b9a8
CMiles9905 <60078979+CMiles9905@users.noreply.github.com>
2020-01-19 17:31:33 -0800

    Update Giant.java
    Added the fee fie foe fum

--- src/Giant.java
+++ src/Giant.java
@@ -1,8 +1,26 @@
 public class Giant extends Critter {
-
-    public Giant(){
+    private int moves=1;
+    public Giant() {
 
     }
 
+    public String toString() {
+        if (moves > 33){
+            moves = 1;
+        }
+        if (moves <= 6 && moves >= 1){
+            return "Fee";
+        }
+        if (moves >=7 && moves <= 19){
+            return "Fie";
+        }if (moves >=20 && moves <= 26) {
+            return "Foe";
+        } else return "Fum";
+    }
 
+
+    public Action getMove(CritterInfo info){
+        moves++;
+    return super.getMove(info);
+    }
 }
